88eba5774daa7e0f5177c7138adf9cdad7a49f42
Mahima Gupta <mahimagupta1195@gmail.com>
2019-08-09 18:11:49 -0700

    Update ksmallest.java

--- ksmallest.java
+++ ksmallest.java
@@ -2,7 +2,7 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
       if(matrix==null || matrix.length==0)
           return -1;
-        else{
+       
             PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
             for(int i=0;i<matrix.length;i++){
                 for(int j=0;j<matrix[0].length;j++){
@@ -16,6 +16,6 @@ class Solution {
                 k--;
             }
             return pq.poll();
-        }
+        
     }
 }
