f3c32f814c6ae289d4b8017b67b6fe949466c2e8
Rakshitha94 <rakshithalr3007@gmail.com>
2019-11-21 23:08:49 -0800

    REVIEW

--- mergelist_3.py
+++ mergelist_3.py
@@ -1,23 +1,32 @@
 #explaination: put all the heads of all the lists in the heap of size k(number of lists). if adding an element exceeds the size, smallest element will be popped because of min heap property and if that value, linked list formation is done
 #time complexity: o(nlogk)
-#Leetcode: error typer error: '<' not supported between instances of listnode and listnode
-from queue import PriorityQueue
+#Leetcode: error
+
+
+import heapq
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     def mergeKLists(self, lists):
-        queue = PriorityQueue()
+        queue = []
+        curr = ListNode(0)
+        ans = curr
         for head in lists:
             if head:
-                queue.put((head.val, head))
+                print(head.val, head)
+                heapq.heappush(queue, (head.val, head))
         curr = ans = ListNode(0)
-        while not queue.empty():
-            curr.next = queue.get()[1]
+
+        while len(queue) != 0:
+            minimum = heapq.heappop(queue)[1]
+            curr.next = minimum
             curr = curr.next
             if curr.next:
-                queue.put((curr.next.val, curr.next)
+                heapq.heappush(queue, (curr.next.val, curr.next))
         return ans.next
+
