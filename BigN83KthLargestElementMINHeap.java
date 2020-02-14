5a46ac7b1984e63da640dba1f4144ca9a32d2f85
Sanjay Vishwakarma <svishwak@andrew.cmu.edu>
2020-02-13 23:59:13 -0800

    K th Largest Element Min Heap

---
+++ BigN83KthLargestElementMINHeap.java
@@ -0,0 +1,20 @@
+//Time complexity is O(nlogk)
+//Space complexity is O(k)
+//This solution is submitted on leetcode
+
+import java.util.PriorityQueue;
+
+public class BigN83KthLargestElementMINHeap {
+	class Solution {
+		public int findKthLargest(int[] nums, int k) {
+			PriorityQueue<Integer> pq = new PriorityQueue<>();
+			for (int i = 0; i < nums.length; i++) {
+				pq.add(nums[i]);
+				if (pq.size() > k) {
+					pq.poll();
+				}
+			}
+			return pq.poll();
+		}
+	}
+}
