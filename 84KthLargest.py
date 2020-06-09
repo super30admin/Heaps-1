7385f12ebcef349eec82441e35c4328fcaad04e8
aditya <email2aaditya@gmail.com>
2020-06-09 01:39:50 -0700

    Heaps-1

--- 84KthLargest.py
+++ 84KthLargest.py
@@ -1,11 +1,14 @@
 """
-T = O(nlogk)
-S = O(k)
+    // Time Complexity :T = O(nlogk)
+    // Space Complexity :S = O(k)
+    // Did this code successfully run on Leetcode : Need to design code as per LC
+    // Any problem you faced while coding this : NA
 
-create heap of size k
-push numbers from nums till the heap is full
-if len(heapifyqueue) > k, pop element so that kth element is always on the top
-at the end, return top element of the heap i.e kth element
+    //Explanation:
+    create heap of size k
+    push numbers from nums till the heap is full
+    if len(heapifyqueue) > k, pop element so that kth element is always on the top
+    at the end, return top element of the heap i.e kth element
 """
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
