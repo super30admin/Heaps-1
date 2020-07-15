ced3a084d33f6170bada6fa79c1cd73fa19d2dac
RamaAncha <66054290+RamaAncha@users.noreply.github.com>
2020-07-14 23:54:49 -0400

    Create KthLargest.java

---
+++ KthLargest.java
@@ -0,0 +1,15 @@
+Executed in leet code- yes
+Time Complexity - k logk
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i:nums){
+            pq.add(i);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
