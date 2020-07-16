2107440ecc7591ae9bb36c994216492a2d364290
Dinesh <dineshnadar95@gmail.com>
2020-07-15 22:05:12 -0400

    Completed

---
+++ MergeListNode.java
@@ -0,0 +1,38 @@
+//Time complexity=O(nklog k)
+//Space complexity=O(k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0)return null;
+        ListNode dummy = new ListNode(-1);
+        ListNode result = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for(ListNode head: lists){
+            if(head != null){
+                pq.add(head);
+            }
+        }
+        while(!pq.isEmpty()){
+            ListNode min = pq.poll();
+            dummy.next = min;
+            dummy = dummy.next;
+            if(min.next != null){
+                min = min.next;
+                pq.add(min);
+            }
+        }
+        return result.next;
+    }
+}
---
+++ kthlargestElement(minHeap).java
@@ -0,0 +1,17 @@
+//Time complexity=O(N log k)
+//Space complexity=O(k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No 
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < nums.length; i++){
+            pq.add(nums[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
