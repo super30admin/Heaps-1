6115334dd2fb106f58db3db9f556ac3fc76d8e51
VirajDeshmukh1 <59378815+VirajDeshmukh1@users.noreply.github.com>
2020-07-24 00:10:46 -0700

    Add files via upload

---
+++ merge-k-sorted-lists.java
@@ -0,0 +1,27 @@
+/*
+    Time Complexity : O(nlogn)
+    Space Complexity : O(n)
+*/
+
+
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue <Integer> minHeap = new PriorityQueue<>();
+        
+        for(ListNode head : lists){
+            while(head!=null){
+                minHeap.add(head.val);
+                head=head.next;
+            }
+        }
+        ListNode dummy=new ListNode(-1);
+        ListNode head = dummy;
+        while(!minHeap.isEmpty()){
+            head.next=new ListNode(minHeap.remove());
+            head=head.next;
+        }
+        return dummy.next;
+    }
+}
+
