a22dd8215ac1a231140d154aa6a82f53fa32ce10
shashidhar-01 <66799707+shashidhar-01@users.noreply.github.com>
2020-08-20 01:07:59 -0500

    Update mergeKSortedLists.java

--- mergeKSortedLists.java
+++ mergeKSortedLists.java
@@ -39,9 +39,11 @@ class Solution {
     }
 }
 **/
+
 //time complexity: O(nklogk) here we are taking a priority queue whose size is equal to the number of linked lists , even during the implementation of the while loop.
 //n is the avg length of the linked lists, k is the number of linked lists
 //space complexity: O(k) ; at any time, we will have a max of k number of nodes in the minheap.
+/**
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> minheap = new PriorityQueue<>((a,b)->a.val-b.val); //we will make min heap based on node values
@@ -58,6 +60,55 @@ class Solution {
             if(min.next!=null) minheap.add(min.next);
         }
         return dummyhead.next;
+    }
+}
+**/
+
+//time complexity: O(n* (k^2))
+//space complexity:  O(1)
+//executed on leetcode: yes
+//approach: merge two lists and merge the resultant list with next list and so on
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0) return null;
+        ListNode merged = null;
+        for(ListNode head: lists)
+        {
+             merged =  merged(merged,head); //merged list will be a combination of earlier merged list and the current list
+        }
+        return merged;
+    }
+    
+    private ListNode merged(ListNode head1,ListNode head2)
+    {   if(head1==null) return head2;
+        if(head2==null) return head1;
+        ListNode dummy = new ListNode(-1);
+        ListNode newHead = dummy;
+        while(head1!=null && head2!=null)
+        {
+            if(head1.val<head2.val)
+            {
+                newHead.next = head1;
+                head1 = head1.next;
+                
+            }
+            else
+            {
+                newHead.next = head2;
+                head2 = head2.next;
+            }
+            newHead=newHead.next;
+        }
+        if(head1==null)
+        {
+            newHead.next = head2;
+        }
+     else
+     {
+         newHead.next = head1;
+     }
+     return dummy.next;
     }
 }
 
