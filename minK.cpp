d3f3a090b8678f1ece4a33de8cdc926702847b50
Jaisurya Nanduri <jaisunanduri1@gmail.com>
2020-10-28 22:45:26 -0500

    Finished Heap 1

---
+++ minK.cpp
@@ -0,0 +1,22 @@
+// Time - O(NlogK)
+// Space - O(K)
+// Leetcode -yes
+// Problems - no
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int num : nums)
+        {
+            pq.push(num);
+            if(pq.size() > k)
+            {
+                pq.pop();
+            }
+        }
+        
+        return pq.top();
+        
+    }
+};
