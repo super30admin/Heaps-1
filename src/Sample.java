bb98fb30270fb83ab066feacc9540740b3a60dcf
pkariya1 <pkariya1@binghamton.edu>
2020-11-29 14:10:27 +0530

    Kth largest

---
+++ src/Sample.java
@@ -0,0 +1,20 @@
+// Using Min heap
+// Time Complexity : O(nlog(k))
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+//
+
+
+// Using Max heap
+// Time Complexity : O(nlog(n-k))
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+
+
+// Your code here along with comments explaining your approach
+//
