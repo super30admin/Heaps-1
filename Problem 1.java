6e5904226b21d6ac1459fadc2726d46962475fcd
Juhi Madhwani <jhmadhwa@ncsu.edu>
2021-02-02 20:10:50 -0500

    Completed Problem 1

--- Problem 1.java
+++ Problem 1.java
@@ -21,3 +21,30 @@ class Solution {
         return pq.peek();
     }
 }
+
+
+
+//Time Complexity: O(N log (N-k)) where N is the length of nums array 
+//Space Complexity: O(N-k)
+
+//Successfully runs on leetcode: 3 ms
+
+//Approach: We are using Max-heap to keep a track of N-k elements at a time. So, in a man-heap, at a time, there will be only N-k
+//elements that will be the smaller elements than the element which is at N-k+1 index after heapify is done and that element
+//will be removed from the heap as the size of the heap becomes greater than N-k. During polling of the extra element, we will
+//keep a track of minimum value which in the end will be the kth largest element.
+
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
+        int min = Integer.MAX_VALUE;
+        for(int n : nums)
+        {
+            pq.add(n);
+            if(pq.size() > nums.length - k)
+                min = Math.min(min,pq.poll());     
+        }
+        return min;
+    }
+}
