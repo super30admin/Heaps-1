79dadd312ba60de33af51cac98261cce6d65628e
soumith <soumithr@buffalo.edu>
2021-03-07 18:11:27 -0500

    Finished Task

---
+++ kthLargestNumber.py
@@ -0,0 +1,12 @@
+﻿#TimeComplexity:O(Nlogk) 
+#SpaceComplexity: O(k)
+#Did this code successfully run on Leetcode : Yes 
+#Any problem you faced while coding this : No
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap=[]
+        for i in range(len(nums)):
+            heapq.heappush(heap,nums[i])
+            if i >= k:
+                heapq.heappop(heap)
+        return heapq.heappop(heap)
