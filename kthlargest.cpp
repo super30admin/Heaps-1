3c68409491461e7b19e1c4d4bd859819ccaba227
pratikshakapse5 <40233432+pratikshakapse5@users.noreply.github.com>
2021-07-06 17:18:31 -0700

    Update kthlargest.cpp

--- kthlargest.cpp
+++ kthlargest.cpp
@@ -1,1 +1,15 @@
 
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+         priority_queue<int> pq;
+        for(auto i:nums){
+            pq.push(i);
+        }
+        while(k>1){
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
