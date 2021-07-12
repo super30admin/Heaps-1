ed6bc65e9a0dc4930ae061cafb59efd9c5f197cb
nishigandhamhaisne <nishmhaisne@gmail.com>
2021-07-12 16:49:24 -0700

    updated comments

--- k_largest_element.py
+++ k_largest_element.py
@@ -11,13 +11,15 @@ class Solution:
         for i in range(k):
             heapq.heappush(heapm, nums[i])
         start = k
-        #         Iterate over the list and if any other number is greater than top element in heap then we will pop the root and add the current element from the list and heapify the heap
+        # Iterate over the list and if any other number is greater than
+        # top element in heap then we will pop the root and add the current element from the list and heapify the heap
         for i in range(start, len(nums)):
             if nums[i] > heapm[0]:
                 heapq.heappop(heapm)
                 heapq.heappush(heapm, nums[i])
                 heapq.heapify(heapm)
-        #         In the end we will return the kth largest number which will be at the root of the heap we builded
+        #         In the end we will return the kth largest number which will be at
+        #         the root of the heap we built
         return heapm[0]
 
 
