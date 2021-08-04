9ae97f3a304715f225d423c7263af214b17447aa
Nyrikabr <nyrikabr@gmail.com>
2021-08-05 00:32:06 +0530

    Completed Sprint Heaps-1

---
+++ Find_kth_Largest_Element_using_Max_Heap.java
@@ -0,0 +1,30 @@
+// Time Complexity : O(N log (n-k))
+// Space Complexity : O(n-k)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach
+
+/**
+ * Implementing using Max Heap
+ */
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0)
+            return Integer.MIN_VALUE;
+        
+        //max heap initialization
+        PriorityQueue<Integer> pqueue = new PriorityQueue<>((a,b) -> (b-a));
+        
+        int result = Integer.MAX_VALUE;
+        
+        for(int num: nums){
+            pqueue.add(num);
+            if(pqueue.size() > nums.length - k)
+                result = Math.min(pqueue.poll(), result);
+        }
+        
+        return result;
+    }
+}
