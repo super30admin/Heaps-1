eb674a97d5fd290fffdf387cceacd17f980ae0c5
Durgesh Patil <patildurgesh1998@gmail.com>
2021-09-02 18:34:31 -0400

    Create Problem-1.cpp

---
+++ Problem-1.cpp
@@ -0,0 +1,43 @@
+/*
+Min Heap Approach
+Time Complexity = O(NLog k)
+Space Complexity = O(K)
+where N is the number of elements in the array and k is the height of the heap.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int, vector<int>, greater<int>> pq;
+        for(int i = 0; i<nums.size();i++)
+        {
+            pq.push(nums[i]);
+            if(pq.size()>k)
+                pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+
+/*
+Max Heap Approach
+Time Complexity = O(NLog k)
+Space Complexity = O(N-K)
+where N is the number of elements in the array and k is the height of the heap.
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue <int> pq;
+        int result = INT_MAX;
+        for(int i = 0; i<nums.size();i++)
+        {
+            pq.push(nums[i]);
+            if(pq.size()>nums.size()-k){
+                result = min(result, pq.top());
+                pq.pop();
+            }
+        }
+        return result;
+    }
+};
