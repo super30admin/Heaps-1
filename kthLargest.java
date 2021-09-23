9742a1086103721c610b7c899b80a6d8018c9e8c
Sai Sruthi Mareedu <smareed1@sundevils.asu.edu>
2021-09-22 23:45:20 -0700

    Completed

---
+++ kthLargest.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.add(num);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
