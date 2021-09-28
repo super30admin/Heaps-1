247783f9db87dc05b819b7a292313d87492c3146
Anuj <anujdhootr@gmail.com>
2021-09-27 22:10:39 -0500

    KthLargest Heap Solution

---
+++ kthLargest.java
@@ -0,0 +1,25 @@
+/*
+
+Time:  O(NLogK)
+Space: O(K)
+
+N number of elements in array
+*/
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> heap =
+            new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+
+        
+        for (int n: nums) {
+          heap.add(n);
+          if (heap.size() > k)
+            heap.poll();
+        }
+
+        
+        return heap.poll();        
+  }
+}
