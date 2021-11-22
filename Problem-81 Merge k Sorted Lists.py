b4dd0a88a460ac1ae7196fc7772dcd9b5c4654df
Adhish Shrivastava <adhish70@gmail.com>
2021-11-22 09:17:03 -0500

    Initial Commit

---
+++ Problem-81 Merge k Sorted Lists.py
@@ -0,0 +1,16 @@
+# 23. Merge k Sorted Lists
+# https://leetcode.com/problems/merge-k-sorted-lists/
+
+# Logic:
+
+# Time Complexity: 
+# Space Complexity: 
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        pass
---
+++ Problem-82 Kth Largest Element in an Array.py
@@ -0,0 +1,11 @@
+# 215. Kth Largest Element in an Array
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+# Logic:
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pass
