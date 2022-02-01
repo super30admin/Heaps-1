6d7fc23a0b3cfd970bf32e2d209be6378fd8d7be
mokariv <91024259+mokariv@users.noreply.github.com>
2022-02-01 16:53:38 -0500

    solved kth largest element the array

---
+++ KthLargestElement215.java
@@ -0,0 +1,18 @@
+// Time Complexity : O(nlogk): We loop over n elements and performs extractions of log*k complexity.
+// Space Complexity : O(k): heap size
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int n : nums){
+            pq.add(n);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+        
+    }
+}
