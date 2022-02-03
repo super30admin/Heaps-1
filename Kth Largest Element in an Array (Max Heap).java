f7aeb84e93c0cff8d27b565719728013b7ca7afc
aniket14thumar <aniket14.thumar@gmail.com>
2022-02-03 11:32:30 -0800

    Completed Kth Largest Element in an Array Problem (Max Heap)

---
+++ Kth Largest Element in an Array (Max Heap).java
@@ -0,0 +1,22 @@
+// Time Complexity : O(nlog(n-k)); where k is provided as an input
+// Space Complexity : O(k)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        // Maximum Heap - largest element is maintained at the top of the stack
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b -a);
+        int min = Integer.MAX_VALUE;
+        
+        for(int n : nums){
+            pq.add(n);
+            // if heap size exceeds n - k
+            if(pq.size() > nums.length - k){
+                // remove the extra element from the heap and compare it with current min
+                min = Math.min(min, pq.poll());
+            }
+        }
+        return min;
+    }
+}
