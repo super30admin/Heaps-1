31210b90a1aeccea1722da8f7ce1039536d6949a
harshaanirudh <harsha.anirudh@gmail.com>
2022-02-05 13:24:02 -0800

    Update MergeLists.java

--- MergeLists.java
+++ MergeLists.java
@@ -45,3 +45,40 @@ class Solution {
         return temp.next;
     }
 }
+
+// Time Complexity : O(nklog(nk))
+// Space Complexity : O(k)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0)
+            return null;
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(ListNode list:lists){
+            if(list!=null)
+                pq.add(list);
+            
+        }
+        while(!pq.isEmpty()){
+            ListNode min=pq.poll();
+            curr.next=min;
+            if(min.next!=null){
+                pq.add(min.next);
+            }
+            curr=curr.next;
+        }
+        return dummy.next;
+    }
+}
