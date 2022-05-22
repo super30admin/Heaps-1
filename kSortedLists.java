4f7b5ea412a98aeb1e479772b326e0112a1d329e
r-ashwini <34915478+r-ashwini@users.noreply.github.com>
2022-05-21 23:09:41 -0700

    Update kSortedLists.java

--- kSortedLists.java
+++ kSortedLists.java
@@ -62,5 +62,98 @@ class Solution {
 }
 }
 
+
+
+
+
+TCO(n * k logk) SC O(1)
+Intuition & Algorithm
+
+This approach walks alongside the one above but is improved a lot. We don't need to traverse most nodes many times repeatedly
+
+Pair up \text{k}k lists and merge each pair.
+
+After the first pairing, \text{k}k lists are merged into k/2k/2 lists with average 2N/k2N/k length, then k/4k/4, k/8k/8 and so on.
+
+Repeat this procedure until we get the final sorted linked list.
+
+Thus, we'll traverse almost NN nodes per pairing and merging, and repeat this procedure about \log_{2}{k}log 
+2
+​
+ k times.
+ 
+ 
+ class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        
+        int k = 1;
+        while (k < lists.length)
+        {
+            // 0, 1, 2, 3
+            // 0, 1, 2
+            // 0, 1, 2, 3, 4, 5, 6, 7, 8
+            //System.out.println("lists length is: " + lists.length);
+            for (int i = 0; (i + k) < lists.length; i+= 2 * k)
+            {
+                /*System.out.println("i is: " + i);
+                System.out.println("i+k is: " + (i+k));
+                if (lists[i] == null)
+                {
+                    System.out.println("lists[i] is: null");
+                } else
+                    System.out.println("lists[i+k] is: " + lists[i].val);
+                if (lists[i+k] == null)
+                {
+                    System.out.println("lists[i] is: null");
+                } else
+                    System.out.println("lists[i+k] is: " + lists[i+k].val);
+                */
+                /*
+                lists[i] = mergeTwoSortedList(lists[i], lists[i+k]);
+                
+            }
+            // increase gap for each iteration
+            // 1 -> 2 -> 4 -> 8
+            k *= 2;
+        }
+
+        return lists[0];
+    }
+    
+    public ListNode mergeTwoSortedList(ListNode l1, ListNode l2)
+    {
+        if (l1 == null)
+            return l2;
+        
+        if (l2 == null)
+            return l1;
+        
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode ans = new ListNode();
+        ListNode curr = ans;
+        while (p1 != null && p2 != null)
+        {
+            if (p1.val < p2.val)
+            {
+                curr.next = p1;
+                curr = curr.next;
+                p1 = p1.next;
+            } else
+            {
+                curr.next = p2;
+                curr = curr.next;
+                p2 = p2.next;
+            }
+        }
+        
+        curr.next = p1 == null ? p2 : p1;
+        
+        return ans.next;
+    }
+}
+*/
 */
 
