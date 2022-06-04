d76895e393638d0dccad23f612531ee9338cd9d2
Hruda Mohod <hruda.mohod@gmail.com>
2022-06-04 18:35:39 -0400

    Completed Homework 06/03 Problems - 81,82

---
+++ mergeksortedlists.java
@@ -0,0 +1,35 @@
+// Time Complexity :O(nklogk) - n=lists size
+// Space Complexity : O(k) 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val-b.val);
+        for(ListNode head:lists){
+            pq.add(head);
+        }
+        
+        ListNode result = new ListNode(-1);
+        ListNode curr = result;
+        while(!pq.isEmpty()){
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+            if(min.next!=null){
+                pq.add(min.next);
+            }
+        }
+        
+        return result.next;
+    }
+}
