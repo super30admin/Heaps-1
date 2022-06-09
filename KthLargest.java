3234011fb1f7367a5aa2b2f63398da6708e26e79
Kalvakolanu, Anjaneya Teja Sarma <anjaneya.teja.sarma.kalvakolanu@ADP.com>
2022-06-09 01:15:24 -0700

    Heaps-1 submitted please review

---
+++ KthLargest.java
@@ -0,0 +1,15 @@
+
+//time - O(nlogk)
+//space - O(1)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num: nums){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.poll();
+    }
+}
