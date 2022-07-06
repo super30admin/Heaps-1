2cbe88c0b88d44e513a5169f2dab5c4d5dad4f97
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-06 11:02:24 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -74,3 +74,53 @@ class Solution {
         return pq.poll();
     }
 }
+
+//****MERGE K-SORTED LISTS****
+//Time complexity:nlogk where n is the total nodes and logk is of insertion of k nodes at particular time in the heap.
+//Space complexity:0(k)
+//Leetcode runnable:Y
+//Any doubts:N
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        ListNode result=new ListNode(-1);
+        ListNode curr=result;
+        
+        //Adding the heads to the queue.
+        for(ListNode head: lists)
+        {
+            if(head!=null)
+            {
+                pq.add(head);
+            }
+        }
+       
+        //Popping the top node from heap
+        //As that would be the minimum
+        //Parallely adding it to the result
+        //Whichever node is popped looking for its next if it has next, add to pq.
+        while(!pq.isEmpty())
+        {
+            ListNode min=pq.poll();
+            curr.next=min;
+            curr=curr.next;
+            if(min.next!=null)
+            {
+                pq.add(min.next);
+            }
+        }
+        
+        return result.next;
+    }
+}
