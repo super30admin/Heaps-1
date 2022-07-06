4945c91c03b5e91a7b21e7f9aa1588f57cdb2e25
Prasad Gagarani <52399176+prasadgagarani@users.noreply.github.com>
2022-07-06 10:18:03 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,4 +1,5 @@
-//****Kth largest element in an array****
+//****Kth LARGEST ELEMENT IN ARRAY USING MIN HEAP****
+//KEEP SIZE OF HEAP AS K;
 //Time complexity:nlogk where n is the size of array and k is the size of heap
 //Space complexity:0(k);
 //Leetcode runnable: Y;
@@ -20,5 +21,31 @@ class Solution {
             }
         }
         return pq.poll();
+    }
+}
+
+//****Kth LARGEST ELEMENT IN ARRAY USING MAX HEAP****
+//KEEP SIZE OF HEAP AS N-K;
+//Time complexity:nlog(n-k) where n is the size of array and k is the size of heap
+//Space complexity:0(n-k);
+//Leetcode runnable: Y;
+//Any doubts: No;
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+        int n=nums.length;
+        int result=Integer.MAX_VALUE;
+        //Iterate through array and add elements to heap
+        for(int num:nums)
+        {
+            pq.add(num);
+            
+            if(pq.size()>n-k)
+            {
+                result=Math.min(result,pq.poll());
+            }
+        }
+        return result;
     }
 }
