e48029da976487ef695f558f63fb938fad7f5987
Pavan Reddy Gottimukkula <46338498+pavanreddy21@users.noreply.github.com>
2022-08-05 10:18:06 +0000

    Heap solutions

---
+++ KthLargest.java
@@ -0,0 +1,19 @@
+// Time Complexity : O(Nlog(K))
+// Space Complexity : O(k)
+// Did this code successfully run on Leetcode : Yes
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            pq.add(num);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+        return pq.peek();
+    }
+}
