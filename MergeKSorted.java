8266ef0588d3cc2b408aade9fa5af3bd097e27d7
Shivani31996 <shivani.chavan96@gmail.com>
2022-11-06 20:33:37 -0500

    Adding approach 2 Merge K lists

--- MergeKSorted.java
+++ MergeKSorted.java
@@ -41,3 +41,48 @@ public class MergeKSorted {
         return result.next;
     }
 }
+
+/*
+ * Approach 2 - Merging 1st two lists using their heads and then using this merged list to merge with the
+ * third list and so on.
+ */
+*/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode merged = new ListNode(Integer.MIN_VALUE);
+        for(ListNode li:lists)
+        {
+            merged = merge(li,merged);
+        }
+        return merged.next;
+    }
+    
+    public ListNode merge(ListNode h1, ListNode h2)
+    {
+        ListNode result = new ListNode(-1);
+        ListNode curr = result;
+        while(h1 != null && h2 != null)
+        {   
+            if(h1.val < h2.val)
+            {
+                curr.next = h1;
+                h1 = h1.next;
+            }
+            else
+            {
+                curr.next = h2;
+                h2 = h2.next;
+            }
+            curr = curr.next;
+        }
+            if(h1 != null)
+            {
+                curr.next = h1;
+            }
+            if(h2 != null)
+            {
+                curr.next = h2;
+            }
+        return result.next;
+    }
+}
