18d74bbbb8de6051c39dd406d4b95e9353c3aa84
yashikanihalani <yashikanihalani@gmail.com>
2023-01-29 17:54:51 -0500

    Completed Heap-1

---
+++ kthLargestElement.java
@@ -0,0 +1,16 @@
+// Time Complexity : O(nlogk)
+// Space Complexity :O(k)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums==null || nums.length==0) return 0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>k) {
+                pq.poll();
+            }
+        }
+        
+        return pq.poll();
+    }
+}
