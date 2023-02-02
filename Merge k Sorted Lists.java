16a62e96b499bb3c4714127f14bbe30ba0f47dc5
Yash Shirodkar <yashshirodkar@yashs-mbp.mynetworksettings.com>
2023-02-01 20:18:09 -0500

    completed sprint

---
+++ Merge k Sorted Lists.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Time Complexity = O(nklogk)
+// Space Complexity = O(nk)
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+         PriorityQueue <ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for(ListNode node:lists){
+            if(node != null){
+                pq.add(node);
+            }
+        }
+        
+        ListNode result = new ListNode(-1);
+        ListNode curr = result;
+        
+        while(!pq.isEmpty()){
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+            if(min.next != null){
+                pq.add(min.next);
+            }
+        }
+        return result.next;
+        
+    }
+}
