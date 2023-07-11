e2568cc77135d0bd8bfd736010e00d17b7e8017f
Rishabh Pandey <92185359+pandeyrish@users.noreply.github.com>
2023-07-11 03:46:21 -0700

    Sprint-1 completed for Heaps-1

---
+++ MergeKSortedArray.java
@@ -0,0 +1,68 @@
+import java.util.PriorityQueue;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeKSortedArray {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        for (ListNode head : lists) {
+            if (head != null) {
+                pq.add(head);
+            }
+        }
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        MergeKSortedArray solution = new MergeKSortedArray();
+
+        // Create the sample linked lists
+        ListNode[] lists = new ListNode[3];
+
+        ListNode list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode list3 = new ListNode(2, new ListNode(6));
+
+        lists[0] = list1;
+        lists[1] = list2;
+        lists[2] = list3;
+
+        // Merge the sorted lists
+        ListNode mergedList = solution.mergeKLists(lists);
+
+        // Print the merged list
+        printList(mergedList);
+    }
+
+    private static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+}
