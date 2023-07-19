f9ed92ae68f39038185c60d2b01ff6c9fa49cab8
Inderpreet Singh <inderpreetsingh@Inderpreets-MacBook-Pro.local>
2023-07-19 13:25:26 -0700

    Heaps-1

---
+++ Solution2.java
@@ -0,0 +1,34 @@
+// Time Complexity: O(Nlogk)
+// Space Complexity: O(k)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution2 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>( (a,b) -> a.val - b.val);
+        for(ListNode head: lists) {
+            if( head != null ) {
+                pq.add(head);
+            }
+        }
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while( !pq.isEmpty() ) {
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+            if( min.next != null ) {
+                pq.add(min.next);
+            }
+        }
+        return dummy.next;
+    }
+}
