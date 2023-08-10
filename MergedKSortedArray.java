f940f962d235b10ec18644f9600146c1a7587eb4
ayushichourasia02 <ayushichourasia02@gmail.com>
2023-08-09 22:10:36 -0500

    Completed- Problem - 66 - MergedKSortedArray using Min heap

---
+++ MergedKSortedArray.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+
+public class MergedKSortedArray {
+
+    //  Definition for singly-linked list.
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+// Time Complexity :O(nlogk)
+//#Space Complexity :O(n+k)
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> (a.val-b.val));
+            for(ListNode head: lists){
+                if(head!=null){
+                    pq.add(head);
+                }
+            }
+            ListNode dummy = new ListNode(-1);
+            ListNode curr = dummy;
+            while(!pq.isEmpty()){
+                ListNode currMin = pq.poll();
+                curr.next = currMin;
+                curr = curr.next;
+                if(currMin.next!=null){
+                    pq.add(currMin.next);
+                }
+            }
+            return dummy.next;
+
+        }
+    }
+}
