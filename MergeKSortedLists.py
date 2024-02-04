6700b9d7395d8dbb7899e8ae5dbe511225f92abb
Humanvitha Sai Dharani Chinnam <humanvitha.2000@gmail.com>
2024-02-03 20:56:08 -0800

    heaps - 1

--- MergeKSortedLists.py
+++ MergeKSortedLists.py
@@ -1,1 +1,33 @@
 # Problem2 Merge k Sorted Lists(https://leetcode.com/problems/merge-k-sorted-lists/)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+    
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        lessThan = lambda x, y: x.val < y.val
+        ListNode.__lt__ = lessThan
+
+        minHeap = []
+        dummyHead = ListNode(-1)
+        cursorNode = dummyHead
+
+        for i in range(len(lists)):
+            current = lists[i]
+            if current != None:
+                heappush(minHeap,current)
+        
+        while len(minHeap) > 0:
+            minNode = heappop(minHeap)
+            cursorNode.next = minNode
+            if (minNode.next != None):
+                heappush(minHeap, minNode.next)
+            cursorNode = minNode
+        return dummyHead.next
+# Time complexity O(NlogK)
+# Space complexity O(k)
+# K is the no:of linked lists
+# N is the no:of nodes across all linked lists
